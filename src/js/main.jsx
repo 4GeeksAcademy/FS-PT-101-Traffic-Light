d06ec239493c25d8ebf4dbbd535ab1a55f5e7486
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-04-07 17:16:29 +0000

    proyecto terminado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { useState } from 'react';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,8 +12,30 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+
+function Semaforo(){
+
+  const [index, setIndex] = useState(0);
+  let arrayColors = ["colorRed","colorOrange","colorGreen"]
+
+function aumentarIndex(){
+  if (index === 2){
+   setIndex(0)
+  }
+  else{
+ setIndex(index +1)
+  }
+}
+
+  
+
+return (
+  <div className='alineadoCentro'>
+    <button onClick={aumentarIndex}>
+      <Home color={arrayColors[index]}/>
+    </button>
+  </div>
+);
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<Semaforo />);
